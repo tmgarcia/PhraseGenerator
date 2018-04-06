7f6f8a1416560cbdaa4a741711ab382a013f21f0
tmgarcia <tclustuff@gmail.com>
2018-04-05 23:17:38 -0700

    Update console program, templates

--- PhraseGenerator/Program.cs
+++ PhraseGenerator/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace PhraseGenerator
 {
@@ -10,8 +7,39 @@ namespace PhraseGenerator
     {
         static void Main(string[] args)
         {
-            string phrase = PhraseGenerator.Instance.GeneratePhraseFromTemplateFile("HotelCalifornia.txt");
-            Console.WriteLine(phrase);
+            var programRunning = true;
+            while (programRunning)
+            {
+                Console.WriteLine("> Enter template file name or X to exit.\n\t");
+                var input = Console.ReadLine();
+                if (input.ToLower() == "x")
+                {
+                    programRunning = false;
+                    return;
+                }
+
+                var template = input;
+                if (!template.StartsWith("Templates\\"))
+                    template = "Templates\\" + template;
+                if (!Path.HasExtension(template))
+                    template = template + ".txt";
+
+
+                if (!File.Exists(template))
+                {
+                    Console.WriteLine(string.Format("> Could not find the template specified: {0} does not exist.", template));
+                    Console.WriteLine();
+                    continue;
+                }
+
+                Console.WriteLine("\n> Generating text from template...");
+                string phrase = PhraseGenerator.Instance.GeneratePhraseFromTemplateFile(template);
+                Console.WriteLine("> Generation complete.\n");
+                Console.WriteLine(phrase);
+                Console.WriteLine();
+            }
+            
         }
+
     }
 }
